ec4f56822a61f9ce0ade743c67532d343d943af7
MakSimKlim <83maxomus@mail.ru>
2025-04-12 17:47:24 +0500

    JavaFX start

--- AcademyFX/src/main/java/org/example/academyfx/HelloController.java
+++ AcademyFX/src/main/java/org/example/academyfx/HelloController.java
@@ -1,14 +1,26 @@
 package org.example.academyfx;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class HelloController {
+    @FXML
+    private TextField lastName;
+    @FXML
+    private TextField firstName;
+    @FXML
+    private DatePicker age;
+
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        String data = lastName.getText() + " " + firstName.getText() + " " + age.getValue();
+        welcomeText.setText(data);
+        //welcomeText.setText("Welcome to JavaFX Application!");
+        Alert alert = new Alert(Alert.AlertType.INFORMATION, data, ButtonType.OK);
+        alert.setTitle("Hello, " + firstName.getText());
+        alert.show();
     }
 }
