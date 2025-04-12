307d4e4be15741683a1e0e92017a7242af1ad14a
MakSimKlim <83maxomus@mail.ru>
2025-04-12 18:49:13 +0500

    JavaFX connecting to DataBase DONE

--- AcademyFX/src/main/java/module-info.java
+++ AcademyFX/src/main/java/module-info.java
@@ -10,6 +10,7 @@ module org.example.academyfx {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires java.sql;
 
     opens org.example.academyfx to javafx.fxml;
     exports org.example.academyfx;
