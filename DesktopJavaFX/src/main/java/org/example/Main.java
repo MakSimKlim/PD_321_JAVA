ec6c6ed0c0ec247045c0265052938859def14081
MakSimKlim <83maxomus@mail.ru>
2025-04-12 16:45:29 +0500

    Connect JavaFX DONE

--- DesktopJavaFX/src/main/java/org/example/Main.java
+++ DesktopJavaFX/src/main/java/org/example/Main.java
@@ -3,11 +3,17 @@ package org.example;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+//*** Как установить JavaFX ***
+//скачать JavaFX SDK с офф.сайта https://gluonhq.com/products/javafx/
+//Run->Edit Configuration ->  Add "Application"
+//VM Options вставить (с указанием пути распаковки JavaFX SDK):
+// --module-path "E:\Parents\Soft\JAVA\javafx-sdk-24\lib" --add-modules javafx.controls,javafx.fxml --enable-native-access=javafx.graphics --add-exports=javafx.graphics/com.sun.glass.utils=ALL-UNNAMED
+
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage)throws Exception
     {
-
+        System.out.println("Hello, JavaFX!");
     }
     public static void main(String[] args) {
         launch(args);
