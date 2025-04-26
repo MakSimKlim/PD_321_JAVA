0ec7f92b85f36fe5b2890b149ddf46906192fff2
MakSimKlim <83maxomus@mail.ru>
2025-04-26 16:26:32 +0500

    SpringJPAMovies project CREATED

---
+++ SpringJPAMovies/src/main/java/com/example/springjpamovies/SpringJpaMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjpamovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaMoviesApplication.class, args);
+	}
+
+}
